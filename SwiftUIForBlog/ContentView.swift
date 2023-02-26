592820ef12b6122ae163f1849d178baeb4527296
taek0622 <min4644@gmail.com>
2023-02-26 22:20:14 +0900

    Init: Create Project

---
+++ SwiftUIForBlog/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUIForBlog
+//
+//  Created by 김민택 on 2023/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIForBlog/SwiftUIForBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIForBlogApp.swift
+//  SwiftUIForBlog
+//
+//  Created by 김민택 on 2023/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIForBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
